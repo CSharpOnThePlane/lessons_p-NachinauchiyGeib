f769b0723188126d214933b665fc7437771c81df
NachinauchiyGeib <superhoroshiy@mail.ru>
2018-03-14 18:12:49 +0300

    Yee

---
+++ 23. Strings/CS 1.6/CS 1.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напиши сколько звёздочек будет на флаге Ком. Китая а то отправим к националистам.");
+             int y = int.Parse(Console.ReadLine());
+            string zvezdochki = "";
+            for (int i = 0; i < y; i++)
+            {
+                zvezdochki=zvezdochki + "*";
+                
+            }
+            Console.WriteLine(zvezdochki);
+        }
+    }
+}
---
+++ 23. Strings/ConsoleApp39/ConsoleApp39/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Global._Offencife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напиши гимн для Ком. Китая а то отправим к империалистам.");
+            string y = Console.ReadLine();
+            int i = y.Length;
+            string zvezdochki = "";
+            for (int x = 0; x < i; x++)
+            {
+                zvezdochki = zvezdochki + "*";
+
+            }
+            Console.WriteLine(zvezdochki);
+        }
+    }
+}
